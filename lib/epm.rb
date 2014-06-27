89f42334dea2638723c1e3f15c21ec29dcd2b76f
Jasper den Ouden <o.jasper@gmail.com>
2014-06-28 01:15:33 +0200

    This should work, should probably put lib/epm/rpc.rb stuff in its own module

--- lib/epm.rb
+++ lib/epm.rb
@@ -54,6 +54,32 @@ module EPM
     EPM::Query.new(address, position, settings).query
   end
 
+  def rpc_from_args name, method, args, opts
+    params = {}
+    for a in method  # Go through all arguments, and use them.(keywords before real)
+      val = (opts.send(a[0]) or args.shift)
+      if val != nil && val != ''
+        case a[1]  # Respond to kind of parameter.
+        when :d, :p, :v, :i
+          params[a[0]] = val
+        when :a
+          params[a[0]] = (EPM::HexData.hex_guard val)
+        else
+          raise 'Unidentified kind of data'
+        end
+      else
+        params[a[0]] = rpc_default(name, a[0], params)
+      end
+    end
+    return params
+  end
+
+  def rpc args, opts
+    name = args.shift
+    arg_list = rpc_arg_list name
+    return post_rpc_with_params name, (rpc_from_args name, arg_list, args, opts)
+  end
+
   def version
     return VERSION
   end
